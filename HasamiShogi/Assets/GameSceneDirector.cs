93ee845edde463f46979e94ffa0a39473304dabf
yosuke <yosuke20sasami@gmail.com>
2025-01-08 23:15:14 +0900

    盤面とタイルの作成

--- HasamiShogi/Assets/GameSceneDirector.cs
+++ HasamiShogi/Assets/GameSceneDirector.cs
@@ -35,6 +35,42 @@ public class GameSceneDirector : MonoBehaviour
 
         tiles = new GameObject[TILE_X, TILE_Y];
         units = new GameObject[TILE_X, TILE_Y];
+
+        for (int i = 0; i < TILE_X; i++)
+        {
+            for (int j = 0; j < TILE_Y; j++)
+            {
+                // タイルとユニットのポジション
+                float x = i - TILE_X / 2;
+                float y = j - TILE_Y / 2;
+
+                Vector3 pos = new Vector3(x, 0, y);
+
+                // タイル作成
+                GameObject tile = Instantiate(prefabTile, pos, Quaternion.identity);
+
+                // ユニット作成
+                GameObject unit = null;
+
+                // 手前側の駒を作成
+                if (j == 0)
+                {
+                    pos.y += 1;
+                    unit = Instantiate(prefabUnit, pos, Quaternion.identity);
+                }
+
+                // 奥川の駒を作成
+                if (j == TILE_Y -1)
+                {
+                    pos.y += 1;
+                    unit = unit = Instantiate(prefabUnit, pos, Quaternion.identity);
+                }
+
+                // 内部データセット
+                tiles[i, j] = tile;
+                units[i, j] = unit;
+            }
+        }
     }
 
     // Update is called once per frame
